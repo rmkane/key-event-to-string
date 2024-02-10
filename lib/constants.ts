f2dd2f9461b6bc3aa836b3d644e5ca8c2674e592
rmkane <rmkane89@gmail.com>
2024-02-09 20:54:42 -0500

    Add eslint

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,63 +1,63 @@
-import { KeyLookup, Options } from "./types";
+import { type KeyLookup, type Options } from './types'
 
-const KEY_SHIFT_LEFT = 16;
-const KEY_CONTROL_LEFT = 17;
-const KEY_ALT_LEFT = 18;
-const KEY_META_LEFT = 91;
-const KEY_META_RIGHT = 93;
-const KEY_OS_LEFT = 224;
+const KEY_SHIFT_LEFT = 16
+const KEY_CONTROL_LEFT = 17
+const KEY_ALT_LEFT = 18
+const KEY_META_LEFT = 91
+const KEY_META_RIGHT = 93
+const KEY_OS_LEFT = 224
 
 const defaultOptions: Options = {
-  cmd: "Cmd",
-  ctrl: "Ctrl",
-  alt: "Alt",
-  shift: "Shift",
-  joinWith: " + ",
-};
+  cmd: 'Cmd',
+  ctrl: 'Ctrl',
+  alt: 'Alt',
+  shift: 'Shift',
+  joinWith: ' + '
+}
 
-const modifierKeys: Set<number> = new Set([
+const modifierKeys = new Set<number>([
   KEY_ALT_LEFT,
   KEY_CONTROL_LEFT,
   KEY_META_LEFT,
   KEY_META_RIGHT,
   KEY_OS_LEFT,
-  KEY_SHIFT_LEFT,
-]);
+  KEY_SHIFT_LEFT
+])
 
 const keyMap: KeyLookup = {
-  8: "Backspace",
-  9: "Tab",
-  13: "Enter",
-  27: "Escape",
-  32: "Space",
-  36: "Home",
-  33: "Page Up",
-  34: "Page Down",
-  35: "End",
-  37: "Left",
-  38: "Up",
-  39: "Right",
-  40: "Down",
-  46: "Delete",
-  112: "F1",
-  113: "F2",
-  114: "F3",
-  115: "F4",
-  116: "F5",
-  117: "F6",
-  118: "F7",
-  119: "F8",
-  120: "F9",
-  121: "F10",
-  122: "F11",
-  123: "F12",
-  186: ";",
-  187: "=",
-  188: ",",
-  189: "-",
-  190: ".",
-  192: "`",
-  222: "'",
-};
+  8: 'Backspace',
+  9: 'Tab',
+  13: 'Enter',
+  27: 'Escape',
+  32: 'Space',
+  36: 'Home',
+  33: 'Page Up',
+  34: 'Page Down',
+  35: 'End',
+  37: 'Left',
+  38: 'Up',
+  39: 'Right',
+  40: 'Down',
+  46: 'Delete',
+  112: 'F1',
+  113: 'F2',
+  114: 'F3',
+  115: 'F4',
+  116: 'F5',
+  117: 'F6',
+  118: 'F7',
+  119: 'F8',
+  120: 'F9',
+  121: 'F10',
+  122: 'F11',
+  123: 'F12',
+  186: ';',
+  187: '=',
+  188: ',',
+  189: '-',
+  190: '.',
+  192: '`',
+  222: "'"
+}
 
-export { defaultOptions, keyMap, modifierKeys };
+export { defaultOptions, keyMap, modifierKeys }
--- lib/functions.ts
+++ lib/functions.ts
@@ -1,37 +1,56 @@
-import type { EventKey, Modifiers, Options } from "./types";
+import type { EventKey, KeyEventDetails, Modifiers, Options } from './types'
 
-import { keyMap, modifierKeys } from "./constants";
+import { defaultOptions, keyMap, modifierKeys } from './constants'
 
-function buildKeyMap(event: KeyboardEvent): EventKey {
-  const { altKey, ctrlKey, keyCode, metaKey, shiftKey } = event;
-  const isOnlyModifier: boolean = modifierKeys.has(keyCode);
+function buildKeyMap (event: KeyboardEvent): EventKey {
+  const { altKey, ctrlKey, keyCode, metaKey, shiftKey } = event
+  const isOnlyModifier: boolean = modifierKeys.has(keyCode)
   const character = isOnlyModifier
     ? null
-    : keyMap[keyCode] ?? String.fromCharCode(keyCode);
+    : keyMap[keyCode] ?? String.fromCharCode(keyCode)
 
   return {
-    character: character,
+    character,
     modifiers: {
       cmd: metaKey,
       ctrl: ctrlKey,
       alt: altKey,
-      shift: shiftKey,
-    },
-  };
+      shift: shiftKey
+    }
+  }
 }
 
-function buildKeyArray(event: KeyboardEvent, options: Options): string[] {
-  const map: EventKey = buildKeyMap(event);
-  const modifiers: Modifiers = map.modifiers;
-  const result = [];
+function buildKeyArray (event: KeyboardEvent, options: Options): string[] {
+  const map: EventKey = buildKeyMap(event)
+  const modifiers: Modifiers = map.modifiers
+  const result = []
 
-  if (modifiers.cmd) result.push(options.cmd);
-  if (modifiers.ctrl) result.push(options.ctrl);
-  if (modifiers.alt) result.push(options.alt);
-  if (modifiers.shift) result.push(options.shift);
-  if (map.character) result.push(map.character);
+  if (modifiers.cmd) result.push(options.cmd)
+  if (modifiers.ctrl) result.push(options.ctrl)
+  if (modifiers.alt) result.push(options.alt)
+  if (modifiers.shift) result.push(options.shift)
+  if (map.character != null) result.push(map.character)
 
-  return result;
+  return result
 }
 
-export { buildKeyArray, buildKeyMap };
+function details (event: KeyboardEvent): KeyEventDetails {
+  const map: EventKey = buildKeyMap(event)
+  const mods: Modifiers = map.modifiers
+  const hasModifier = mods.cmd || mods.ctrl || mods.alt || mods.shift
+
+  return {
+    hasKey: map.character != null,
+    hasModifier,
+    map
+  }
+}
+
+function event2string (options: Options = defaultOptions): (event: KeyboardEvent) => string {
+  const opts: Options = { ...defaultOptions, ...options }
+  return function (event: KeyboardEvent): string {
+    return buildKeyArray(event, opts).join(opts.joinWith)
+  }
+}
+
+export { details, event2string }
